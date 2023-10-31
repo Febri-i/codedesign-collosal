b122f0cff1239482e0bfb5832963118d99fdc545
Febri-i <febribayun@gmail.comm>
2023-10-31 13:36:12 +0700

    cleanup

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,5 +1,4 @@
 import { StartServer, createHandler, renderAsync } from "solid-start/entry-server";
-import { renderStream } from "solid-start/server/render";
 
 export default createHandler(
   renderAsync((event) => <StartServer event={event} />),
