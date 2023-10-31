9cd452528db52f634ba6ff68ee580271d4e69895
Febri-i <febribayun@gmail.comm>
2023-10-31 12:13:47 +0700

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import solidStartVercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 
 import { defineConfig } from "vite";
+import vercel from "solid-start-vercel";
 export default defineConfig({
   plugins: [
     solid({
-      adapter: solidStartVercel(),
       ssr: false
     }),
   ],
